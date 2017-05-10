a95adc5bba243f892bede7b597844d8a761ee5f9
artadaka <artadaka>
2017-05-10 11:00:13 -0400

    First commit

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
-
-/**
- * Module dependencies.
- */
-
+'use strict';
 var express = require('express')
 var http = require('http')
 var path = require('path');
 var request=require("request");
 var app = express();
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var PORT = process.env.PORT || 8085;
 
 // development only
 app.post('/provision', function(req, res) {
@@ -37,6 +24,7 @@ app.post('/provision', function(req, res) {
 	    });
 	  
 	});
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(PORT, function () {
+    console.log('listening on port ' + PORT)
+
 });
