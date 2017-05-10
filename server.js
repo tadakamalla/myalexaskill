6d76f0aea778246396946ec247ad5663361fccc1
artadaka <artadaka@TADAKAMALLA-PC.us.oracle.com>
2017-05-10 16:35:57 -0400

    Provision loop

--- server.js
+++ server.js
@@ -10,20 +10,28 @@ var PORT = process.env.PORT || 8085;
 app.post('/provision', function(req, res) {
 	
 	
-	request('http://129.146.35.106:8081/orchestration/bmcs/provision');
+	
+	 request('http://129.146.35.106:8081/orchestration/bmcs/provision',function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		        console.log("Provisioning..");
+		     }
 
-	   	    res.json({
-	      "version": "1.0",
-	      "response": {
-	        "shouldEndSession": true,
-	        "outputSpeech": {
-	          "type": "SSML",
-	          "ssml": "<speak>Provisioning a VM in Baremetal Cloud</speak>"
-	        }
-	      }
-	    });
 	  
-	});
+		})
+		
+ 	    res.json({
+ 		      "version": "1.0",
+ 		      "response": {
+ 		        "shouldEndSession": true,
+ 		        "outputSpeech": {
+ 		          "type": "SSML",
+ 		          "ssml": "<speak>I love Oracle Cloud, your VM will be provisioned momentarily</speak>"
+ 		        }
+ 		      }
+ 		    });
+		  
+		});
+
 app.listen(PORT, function () {
     console.log('listening on port ' + PORT)
 
